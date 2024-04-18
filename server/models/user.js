73c87bc80ba139b8a24ee7e11536d82c54a5fd67
Justin-Barlowe <jbarlowe@my365.bellevue.edu>
2024-04-18 09:11:44 -0400

    Added users model

---
+++ server/models/user.js
@@ -0,0 +1,16 @@
+// user.js
+// user schema and model
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true },
+},
+{ collection: 'users' });
+
+module.exports = mongoose.model('User', UserSchema);
+
+
