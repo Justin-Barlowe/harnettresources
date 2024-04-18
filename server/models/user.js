755b63d7ce810843c09642c6673546c3cc291bf6
Justin-Barlowe <jbarlowe@my365.bellevue.edu>
2024-04-18 09:12:32 -0400

    Updated users schema

--- server/models/user.js
+++ server/models/user.js
@@ -8,6 +8,7 @@ const UserSchema = new Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
   email: { type: String, required: true },
+  role: { type: String, required: true },
 },
 { collection: 'users' });
 
